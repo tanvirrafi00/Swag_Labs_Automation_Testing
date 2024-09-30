66d5a7fbb191d09f55de24d818d80de0d86bc3fa
Tanvir Rafi <tanvirrafi148@gmail.com>
2024-09-30 22:22:14 +0600

    lofin test page added

---
+++ test/specs/loginPageTest.js
@@ -0,0 +1,29 @@
+import { assert, use } from "chai";
+import validLogin from "../../test data/validLogin.json" assert { type: "json" };
+import LoginPage from "../pageObjects/LoginPage.js";
+import ProductsPage from "../pageObjects/ProductsPage.js";
+import Browser from "../../framework/browser/Browser.js";
+import AllureReporter from "@wdio/allure-reporter";
+describe("Check the Login Functionality", () => {
+  it("TC_01:Check the login functionality with accepted usernames and passwords", async () => {
+    AllureReporter.addTestId("TC_01");
+    AllureReporter.addSeverity("Major");
+
+    AllureReporter.addStep("checking login page is opened");
+    assert.isTrue(await LoginPage.isPageOpened());
+    for (let { username, password } of validLogin) {
+      AllureReporter.addStep("set username");
+      await LoginPage.setUsername(username);
+      AllureReporter.addStep("set password");
+      await LoginPage.setPassword(password);
+      AllureReporter.addStep("click login button");
+      await LoginPage.clickLoginButton();
+      AllureReporter.addStep("checking product is opened");
+      assert.isTrue(await ProductsPage.isPageOpened(), "Product page is not opened");
+      AllureReporter.addStep("clicking on menu button");
+      await ProductsPage.clickMenuButton();
+      AllureReporter.addStep("clicking on logout link");
+      await ProductsPage.clickLogoutLink();
+    }
+  });
+});
