60dd0298685bf0b84d47eb0013afc301222b43fd
Tanvir Rafi <tanvirrafi148@gmail.com>
2024-09-30 20:01:10 +0600

    base url added

--- framework/configs/main.wdio.conf.js
+++ framework/configs/main.wdio.conf.js
@@ -1,37 +1,40 @@
-import path from 'node:path';
-import fs from 'fs-extra';
+import path from "node:path";
+import fs from "fs-extra";
 
-export const downloadDir = path.resolve('./tmp');
+export const downloadDir = path.resolve("./tmp");
 
 export const mainConfig = {
-    runner: 'local',
-    exclude: [
-    ],
-    maxInstances: 1,
-    logLevel: 'warn',
-    bail: 0,
-    waitforTimeout: 0,
-    connectionRetryTimeout: 120000,
-    connectionRetryCount: 3,
-    framework: 'mocha',
-    reporters: ['spec'],
-    mochaOpts: {
-        ui: 'bdd',
-        timeout: 60000
-    },
+  runner: "local",
+  exclude: [],
+  maxInstances: 1,
+  logLevel: "warn",
+  bail: 0,
+  waitforTimeout: 0,
+  connectionRetryTimeout: 120000,
+  connectionRetryCount: 3,
+  framework: "mocha",
+  reporters: ["spec"],
+  mochaOpts: {
+    ui: "bdd",
+    timeout: 60000,
+  },
+  baseUrl: "https://www.saucedemo.com/",
 
-    onPrepare: function() {
-        fs.ensureDir(downloadDir);
-    },
+  before: function () {
+    browser.url(this.baseUrl);
+  },
 
-    after: function (result, capabilities, specs) {
-        fs.emptyDir(downloadDir);
-    },
+  onPrepare: function () {
+    fs.ensureDir(downloadDir);
+  },
 
-    afterTest: async function (test, context, { error, result, duration, passed, retries }) {
-        if (!passed) {
-            await browser.takeScreenshot();
-        }
-    },
+  after: function (result, capabilities, specs) {
+    fs.emptyDir(downloadDir);
+  },
 
-}
+  afterTest: async function (test, context, { error, result, duration, passed, retries }) {
+    if (!passed) {
+      await browser.takeScreenshot();
+    }
+  },
+};
