24c134100c9ca256632f1009bc1a40dc691d23c7
Tanvir Rafi <tanvirrafi148@gmail.com>
2024-10-04 09:58:05 +0600

    TC_05 added

--- test/specs/productsPageTest.js
+++ test/specs/productsPageTest.js
@@ -3,6 +3,10 @@ import LoginPage from "../pageObjects/LoginPage.js";
 import dotenv from "dotenv";
 import ProductsPage from "../pageObjects/ProductsPage.js";
 import AllureReporter from "@wdio/allure-reporter";
+import prodcutSortData from "../../test data/productSortData.json" assert { type: "json" };
+import sortedProductList from "../../test data/sortProductList.json" assert { type: "json" };
+import Browser from "../../framework/browser/Browser.js";
+import { mainConfig } from "../../framework/configs/main.wdio.conf.js";
 
 dotenv.config();
 describe("Product page functionality", () => {
@@ -23,5 +27,30 @@ describe("Product page functionality", () => {
 
     AllureReporter.addStep("checking product sort option is displaying");
     assert.isTrue(await ProductsPage.isSortProductDisplayed());
+  });
+
+  it("TC_05:Check whether product sort option AZ is working correctly", async () => {
+    AllureReporter.addStep("opening login page url");
+    await Browser.openUrl(mainConfig.baseUrl);
+    AllureReporter.addStep("checking login page is opened");
+    assert.isTrue(await LoginPage.isPageOpened(), "Login page is not opened");
+
+    AllureReporter.addStep("set username");
+    await LoginPage.setUsername(process.env.USER_NAME);
+
+    AllureReporter.addStep("set password");
+    await LoginPage.setPassword(process.env.PASSWORD);
+
+    AllureReporter.addStep("clcik on login button");
+    await LoginPage.clickLoginButton();
+
+    AllureReporter.addStep("checking product page is opened");
+    assert.isTrue(await ProductsPage.isPageOpened(), "product page is not opened");
+
+    AllureReporter.addStep("select sort value");
+    await ProductsPage.selectSortValue(prodcutSortData["a-z"]);
+
+    AllureReporter.addStep("checking product sorted correctly");
+    assert.isTrue(await ProductsPage.checkIsSortedCorrectly(sortedProductList["a-z"]), "product not sorted a-z");
   });
 });
