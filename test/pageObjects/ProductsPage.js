ca36fbeb6967e5cc007a400e11da37f6071cdb57
Tanvir Rafi <tanvirrafi148@gmail.com>
2024-10-04 09:57:17 +0600

    all products alement added

--- test/pageObjects/ProductsPage.js
+++ test/pageObjects/ProductsPage.js
@@ -1,4 +1,4 @@
-import { Button, Dropdown, Label } from "../../framework/elements/index.js";
+import { Button, Dropdown, ElementsList, Label } from "../../framework/elements/index.js";
 import BasePage from "../../framework/page/BasePage.js";
 import { PreciseTextLocator } from "../../framework/utils/locatorHelper.js";
 
@@ -9,6 +9,7 @@ class ProductsPage extends BasePage {
     this.leftSidebar = new Label(`//div[@class="bm-menu"]`, "Left sidebar");
     this.logoutLink = new Label(`//a[@id="logout_sidebar_link"]`, "Logout Link");
     this.sortProduct = new Dropdown(`//select[@class="product_sort_container"]`, "sort products");
+    this.allProductsName = new ElementsList(Label, `//div[@class="inventory_item_name "]`, "All products");
   }
 
   async clickMenuButton() {
@@ -21,6 +22,22 @@ class ProductsPage extends BasePage {
 
   async isSortProductDisplayed() {
     return await this.sortProduct.state().isDisplayed();
+  }
+
+  async selectSortValue(value) {
+    await this.sortProduct.selectOptionByValue(value);
+  }
+
+  async checkIsSortedCorrectly(sortProductList) {
+    const allProducts = await this.allProductsName.getListOfElements();
+
+    for (let i in allProducts) {
+      const productName = await allProducts[i].getText();
+
+      console.log(sortProductList[i]);
+      if (productName != sortProductList[i]) return false;
+    }
+    return true;
   }
 }
 
